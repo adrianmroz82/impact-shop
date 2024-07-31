0e190687966e8615c4d0eabf3e25c4cef2a92c31
Adrian <adrianmroz82@gmail.com>
2024-07-31 17:05:21 +0200

    fetch products by category

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,3 +1,38 @@
-export default function Category() {
-  return <div>Category</div>;
+import Image from "next/image";
+import { fetchProductsByCategory } from "@/app/lib/api";
+import { Category } from "@/app/lib/model";
+
+import styles from "@/components/product-list.module.css";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Product({ params }: Props) {
+  const category = decodeURIComponent(params.id) as Category;
+  const products = await fetchProductsByCategory(category);
+  const count = products.length;
+
+  return (
+    <div className={styles.categoryContainer}>
+      <h2>
+        Category: {category}, found {count} items
+      </h2>
+      <ul className={styles.productGrid}>
+        {products.map((product) => (
+          <li key={product.id} className={styles.productItem}>
+            <div className={styles.productBox}>
+              <Image src={product.image} alt={product.title} width={500} height={500} className={styles.productImage} />
+              <div className={styles.productInfo}>
+                <div className={styles.productTitle}>{product.title}</div>
+                <div className={styles.productPrice}>${product.price}</div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
