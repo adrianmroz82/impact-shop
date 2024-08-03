fa820368a186917cc55194e661749da1257553a1
Adrian <adrianmroz82@gmail.com>
2024-08-03 14:31:47 +0200

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,44 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/coverage/**",
+    "!<rootDir>/jest.config.ts",
+    // typed redux hooks
+    "!<rootDir>/src/lib/hooks.ts",
+    // models
+    "!<rootDir>/src/lib/model.ts",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleNameMapper: {
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.(css|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
+    "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/__mocks__/fileMock.js`,
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "next/font/(.*)": `<rootDir>/src/__mocks__/nextFontMock.js`,
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/__test__/setupTests.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
+};
+
+export default config;
