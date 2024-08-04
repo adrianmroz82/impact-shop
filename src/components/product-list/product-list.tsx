599f63d64ed61a14c30c27c899d331cf7459f3d8
Adrian <adrianmroz82@gmail.com>
2024-08-04 15:05:02 +0200

    add cart toast

--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -5,6 +5,7 @@ import { Category, Product } from "@/lib/model";
 import { addToCart } from "@/lib/features/cartSlice";
 import { useAppDispatch } from "@/lib/hooks";
 import { Button } from "@/components/button/button";
+import { CartToast } from "@/components/cart-toast/cart-toast";
 
 import styles from "@/components/product-list/product-list.module.css";
 
@@ -37,20 +38,25 @@ export function ProductList({ products, category }: Props) {
                 <Image
                   src={product.image}
                   alt={product.title}
-                  width={500}
-                  height={500}
+                  width={256}
+                  height={256}
                   className={styles.productImage}
                 />
               </div>
               <div className={styles.productInfo}>
-                <div data-testid="product-title" className={styles.productTitle}>{product.title}</div>
-                <div data-testid="product-price" className={styles.productPrice}>${product.price}</div>
+                <div data-testid="product-title" className={styles.productTitle}>
+                  {product.title}
+                </div>
+                <div data-testid="product-price" className={styles.productPrice}>
+                  ${product.price}
+                </div>
                 <Button clickHandler={() => handleAddToCart(product)} text="Add to cart" />
               </div>
             </div>
           </li>
         ))}
       </ul>
+      <CartToast />
     </div>
   );
 }
